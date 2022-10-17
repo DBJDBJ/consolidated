1f86645d99da9659164fd1d7ebd09995d8036717
dbjdbj <dbj@dbj.org>
2022-10-17 23:55:16 +0200

    added

--- azuredotnet/azure_computing_logging_presentation/aws/aws_cloudwatch_agent_ecs.md
+++
@@ -1,6 +0,0 @@
-## CloudWatch Agent And ECS
-
-Cloudwatch Agent need to be configured on EC2
-
-![](aws_media/architecture.png)
-
--- azuredotnet/azure_computing_logging_presentation/splunk_demo_runtime/splunktoken.cmd
+++
@@ -1,8 +0,0 @@
-::
-:: This MUST be given to nginx (or any other) container creation using the splunk log driver
-::
-:: it MUST come from here: https://docs.splunk.com/Documentation/Splunk/8.2.5/Data/UsetheHTTPEventCollector#Configure_HTTP_Event_Collector_on_Splunk_Enterprise 
-:: you install splunk entrprise ONCE and copy paste this token in here
-:: each splunk container removal will require creating and obtaining this token from scratch
-::
-@set "splunktoken=6269b665-ba46-4dfd-9f92-6ec62dc650a0"
